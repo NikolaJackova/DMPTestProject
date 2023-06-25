baefa2cbcdadf13ea8152fc5617503e8c77e28b6
NikolaJackova <nikol.jac@seznam.cz>
2023-06-25 19:35:15 +0200

    Add project files.

---
+++ DMPTestProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DMPTestProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
